9f83db2279a6630f642ee0c913d4973e54b186c1
lun <zhanglun@xunlei.com>
2019-05-15 16:58:08 +0800

    my stack

--- sentry.go
+++ sentry.go
@@ -1,22 +1,11 @@
 package zlutils
 
 import (
-	"bytes"
 	"fmt"
-	"io/ioutil"
-	"runtime"
-
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
 	"net/http/httputil"
 	"strconv"
-)
-
-var (
-	dunno     = []byte("???")
-	centerDot = []byte("·")
-	dot       = []byte(".")
-	slash     = []byte("/")
 )
 
 func Recovery() gin.HandlerFunc {
@@ -24,11 +13,10 @@ func Recovery() gin.HandlerFunc {
 		defer func() {
 			if rec := recover(); rec != nil {
 				endpoint := fmt.Sprintf("%s-%s", c.Request.URL.Path, c.Request.Method)
-				stack := stack(3)
 				httpRequest, _ := httputil.DumpRequest(c.Request, true)
 				logrus.WithFields(logrus.Fields{
 					"httpRequest": string(httpRequest),
-					"stack":       string(stack),
+					"stack":       GetStack(3),
 					"recover":     rec,
 					"endpoint":    endpoint,
 				}).Error()
@@ -40,65 +28,4 @@ func Recovery() gin.HandlerFunc {
 		}()
 		c.Next()
 	}
-}
-
-// stack returns a nicely formated stack frame, skipping skip frames
-func stack(skip int) []byte {
-	buf := new(bytes.Buffer) // the returned data
-	// As we loop, we open files and read them. These variables record the currently
-	// loaded file.
-	var lines [][]byte
-	var lastFile string
-	for i := skip; ; i++ { // Skip the expected number of frames
-		pc, file, line, ok := runtime.Caller(i)
-		if !ok {
-			break
-		}
-		// Print this much at least.  If we can't find the source, it won't show.
-		fmt.Fprintf(buf, "%s:%d (0x%x)\n", file, line, pc)
-		if file != lastFile {
-			data, err := ioutil.ReadFile(file)
-			if err != nil {
-				continue
-			}
-			lines = bytes.Split(data, []byte{'\n'})
-			lastFile = file
-		}
-		fmt.Fprintf(buf, "\t%s: %s\n", function(pc), source(lines, line))
-	}
-	return buf.Bytes()
-}
-
-// source returns a space-trimmed slice of the n'th line.
-func source(lines [][]byte, n int) []byte {
-	n-- // in stack trace, lines are 1-indexed but our array is 0-indexed
-	if n < 0 || n >= len(lines) {
-		return dunno
-	}
-	return bytes.TrimSpace(lines[n])
-}
-
-// function returns, if possible, the name of the function containing the PC.
-func function(pc uintptr) []byte {
-	fn := runtime.FuncForPC(pc)
-	if fn == nil {
-		return dunno
-	}
-	name := []byte(fn.Name())
-	// The name includes the path name to the package, which is unnecessary
-	// since the file name is already included.  Plus, it has center dots.
-	// That is, we see
-	//	runtime/debug.*T·ptrmethod
-	// and want
-	//	*T.ptrmethod
-	// Also the package path might contains dot (e.g. code.google.com/...),
-	// so first eliminate the path prefix
-	if lastslash := bytes.LastIndex(name, slash); lastslash >= 0 {
-		name = name[lastslash+1:]
-	}
-	if period := bytes.Index(name, dot); period >= 0 {
-		name = name[period+1:]
-	}
-	name = bytes.Replace(name, centerDot, dot, -1)
-	return name
 }
