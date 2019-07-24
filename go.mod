a82320d91aea048621aa7b134b75274fe532b81e
lun <zhanglun@xunlei.com>
2019-07-24 17:36:13 +0800

    改成stdout

--- go.mod
+++ go.mod
@@ -9,11 +9,12 @@ require (
 	github.com/go-playground/validator v9.29.0+incompatible
 	github.com/go-sql-driver/mysql v1.4.1
 	github.com/hashicorp/consul/api v1.1.0
-	github.com/lestrrat/go-file-rotatelogs v0.0.0-20180223000712-d3151e2a480f
 	github.com/lun-zhang/gorm v1.9.10-0.20190627121314-6f8ca1642773
 	github.com/prometheus/client_golang v0.9.3
 	github.com/sirupsen/logrus v1.4.2
 	golang.org/x/net v0.0.0-20190514140710-3ec191127204
 	gopkg.in/redis.v5 v5.2.9
-	xlbj-gitlab.xunlei.cn/oversea/zlutils v5.1.0+incompatible
+	zlutils v0.0.0
 )
+
+replace zlutils v0.0.0 => ../zlutils
--- log.go
+++ log.go
@@ -1,18 +1,12 @@
 package zlutils
 
 import (
-	"fmt"
-	"github.com/lestrrat/go-file-rotatelogs"
 	"github.com/sirupsen/logrus"
-	"io"
-	"io/ioutil"
 	"os"
-	"time"
 )
 
 type MyFormatter struct {
 	logrus.JSONFormatter
-	WriterMap map[logrus.Level]io.Writer
 }
 
 func (f MyFormatter) Format(e *logrus.Entry) (serialized []byte, err error) {
@@ -27,57 +21,7 @@ func (f MyFormatter) Format(e *logrus.Entry) (serialized []byte, err error) {
 	e.Time = e.Time.UTC()               //改成UTC时间
 	e.Data["time_unix"] = e.Time.Unix() //方便grep查询范围
 	serialized, err = f.JSONFormatter.Format(e)
-	if err != nil {
-		return
-	}
-	err = f.write(e.Level, serialized)
 	return
-}
-
-func (f MyFormatter) write(level logrus.Level, serialized []byte) (err error) {
-	if writer := f.WriterMap[level]; writer != nil {
-		if _, err = writer.Write(serialized); err != nil {
-			return
-		}
-	}
-	//debug模式
-	if logrus.IsLevelEnabled(logrus.DebugLevel) {
-		//输出到屏幕
-		os.Stderr.Write(serialized)
-		if level != logrus.DebugLevel {
-			//其他日志同时输出到debug.log
-			if writer := f.WriterMap[logrus.DebugLevel]; writer != nil {
-				if _, err = writer.Write(serialized); err != nil {
-					return
-				}
-			}
-		}
-	}
-	return
-}
-
-func getLogWriter(level logrus.Level) *rotatelogs.RotateLogs {
-	dir := fmt.Sprintf("/data/logs/%s", ProjectName)
-	path := fmt.Sprintf("%s/%s.log", dir, level)
-	if _, err := os.Stat(dir); err != nil || os.IsNotExist(err) {
-		panic(fmt.Errorf("not exist dir %s", dir))
-	}
-	writer, err := rotatelogs.New(
-		path+".%Y%m%d",
-		rotatelogs.WithLinkName(path),
-		//修改为地区和时钟为UTC，用于日志回收、命名等操作中用到时间的地方
-		rotatelogs.WithLocation(time.UTC),
-		rotatelogs.WithClock(rotatelogs.UTC), //默认loc，改成UTC
-		//每天分割
-		rotatelogs.WithRotationTime(time.Hour*24), //默认24h
-		//最多3个文件，配合每天分割文件，则是每3天删除旧日志
-		rotatelogs.WithMaxAge(-1),       //默认7*24h，配合次数时需显式设为-1关闭
-		rotatelogs.WithRotationCount(3), //默认-1
-	)
-	if err != nil {
-		panic(err)
-	}
-	return writer
 }
 
 func InitLog(release bool) {
@@ -85,16 +29,6 @@ func InitLog(release bool) {
 		logrus.SetLevel(logrus.DebugLevel) //默认info
 	}
 
-	errorWriter := getLogWriter(logrus.ErrorLevel)
-	logrus.SetFormatter(MyFormatter{
-		WriterMap: map[logrus.Level]io.Writer{
-			logrus.FatalLevel: errorWriter,
-			logrus.PanicLevel: errorWriter,
-			logrus.ErrorLevel: errorWriter,
-			logrus.WarnLevel:  getLogWriter(logrus.WarnLevel),
-			logrus.InfoLevel:  getLogWriter(logrus.InfoLevel),
-			logrus.DebugLevel: getLogWriter(logrus.DebugLevel),
-		},
-	})
-	logrus.SetOutput(ioutil.Discard)
+	logrus.SetFormatter(MyFormatter{})
+	logrus.SetOutput(os.Stdout)
 }
