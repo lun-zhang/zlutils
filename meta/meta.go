084c8b648cea797695bc18a5fc398b057c8010f4
lun <zhanglun@xunlei.com>
2019-09-26 16:42:02 +0800

    u

--- meta/meta.go
+++ meta/meta.go
@@ -2,18 +2,23 @@ package meta
 
 import (
 	"github.com/sirupsen/logrus"
+	"xlbj-gitlab.xunlei.cn/oversea/zlutils/v7/caller"
 )
 
 type Meta map[string]interface{}
 
 //似乎实现了MustGet Set的都可以当做meta
 func (m Meta) MustGet(k string) interface{} {
+	entry := logrus.WithFields(logrus.Fields{
+		"caller": caller.Caller(2),
+		"m":      m,
+	})
 	if m == nil {
-		logrus.Panicf("m is nil")
+		entry.Panic("m is nil")
 	}
 	v, ok := (m)[k]
 	if !ok {
-		logrus.WithField("m", m).Panicf("no key:%s", k)
+		entry.Panic("no value")
 	}
 	return v
 }
