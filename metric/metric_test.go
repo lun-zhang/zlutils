b4aaebad07915c0195d9e0f2e290d7146fd71300
lun <zhanglun@xunlei.com>
2019-11-07 20:45:48 +0800

    暴露GetEndpoint，解决path参数不同而metric变成不同的线的问题

--- metric/metric_test.go
+++ metric/metric_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strings"
 	"testing"
 	"time"
 	"zlutils/code"
@@ -33,8 +34,10 @@ func TestMidRespCounterErr(t *testing.T) {
 	router.Run(":11116")
 }
 
+const projectName = "zlutils"
+
 func TestMidRespCounterLantency(t *testing.T) {
-	const projectName = "zlutils"
+
 	InitDefaultMetric(projectName)
 	router := gin.New()
 	router.Group(projectName).GET("metrics", Metrics) //这样就不会把metric的调用记录下来
@@ -44,4 +47,24 @@ func TestMidRespCounterLantency(t *testing.T) {
 		c.JSON(http.StatusOK, 1)
 	})
 	router.Run(":11117")
+}
+
+//避免path参数不同而metric变成不同的线
+func TestMidPath(t *testing.T) {
+	GetEndpoint = func(c *gin.Context) string {
+		path := c.Request.URL.Path
+		if pre := "/zlutils/path/"; strings.HasPrefix(path, pre) {
+			path = pre + ":id"
+		}
+		return fmt.Sprintf("%s-%s", path, c.Request.Method)
+	}
+	InitDefaultMetric(projectName)
+	router := gin.New()
+	router.Group(projectName).GET("metrics", Metrics) //这样就不会把metric的调用记录下来
+	base := router.Group(projectName, MidRespCounterLatency())
+	base.GET("path/:id", func(c *gin.Context) {
+		time.Sleep(time.Millisecond * 100)
+		c.JSON(http.StatusOK, c.Request.URL.Path)
+	})
+	router.Run(":11118")
 }
