3fc9c54ef1e21bc4c49e027bb5e513daf00763df
zhanglun <zhanglun@xunlei.com>
2020-02-28 15:41:27 +0800

    完善多语言

--- misc/language.go
+++ misc/language.go
@@ -1,6 +1,22 @@
 package misc
 
 const (
-	LangEnglish = "en"
-	LangHindi   = "hi-IN"
+	// 多语言
+	// 文档：http://wiki.xunlei.cn/pages/viewpage.action?pageId=25072987
+	LangEnglish    = "en"
+	LangHindi      = "hi-IN"    //印地语
+	LangMarathi    = "mr-IN"    //马拉地语
+	LangGujarati   = "gu-IN"    //古吉拉特语
+	LangPunjabi    = "pa-IN"    //旁遮普语
+	LangTelugu     = "te-IN"    //泰卢固语
+	LangMalayalam  = "ml-IN"    //马拉雅拉姆语
+	LangTamil      = "ta-IN"    //泰米尔语
+	LangBengali    = "bn-IN"    //孟加拉
+	LangOdia       = "or-IN"    //Oriya
+	LangKannada    = "kn-IN"    //卡纳达语
+	LangAssamese   = "as-IN"    //阿萨姆语
+	LangBhojpuri   = "bho"      //博普里
+	LangHaryanvi   = "Haryanvi" //哈里扬维
+	LangRajasthani = "raj"      //拉贾斯坦尼
+	LangIndonesian = "id-ID"    //印尼语
 )
