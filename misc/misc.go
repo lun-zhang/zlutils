6941daea1e560dfc2d89ba2ca137b404b5e89b51
lun <zhanglun@xunlei.com>
2020-04-28 18:45:59 +0800

    实现min(a,b...) max(a,b...)

--- misc/misc.go
+++ misc/misc.go
@@ -1,5 +1,7 @@
 package misc
 
+import "reflect"
+
 func AbsInt(i int) int {
 	if i >= 0 {
 		return i
@@ -13,3 +15,79 @@ func AbsInt32(i int32) int32 {
 	}
 	return -i
 }
+
+func min(a, b interface{}) interface{} {
+	va := reflect.ValueOf(a)
+	vb := reflect.ValueOf(b)
+	switch va.Kind() {
+	case reflect.Uint:
+	case reflect.Uint8:
+	case reflect.Uint16:
+	case reflect.Uint32:
+	case reflect.Uint64:
+		for i := 0; i < vb.Len(); i++ {
+			bi := vb.Index(i)
+			if va.Uint() > bi.Uint() {
+				va = bi
+			}
+		}
+	default:
+		for i := 0; i < vb.Len(); i++ {
+			bi := vb.Index(i)
+			if va.Int() > bi.Int() {
+				va = bi
+			}
+		}
+	}
+	return va.Interface()
+}
+
+func max(a, b interface{}) interface{} {
+	va := reflect.ValueOf(a)
+	vb := reflect.ValueOf(b)
+	switch va.Kind() {
+	case reflect.Uint:
+	case reflect.Uint8:
+	case reflect.Uint16:
+	case reflect.Uint32:
+	case reflect.Uint64:
+		for i := 0; i < vb.Len(); i++ {
+			bi := vb.Index(i)
+			if va.Uint() < bi.Uint() {
+				va = bi
+			}
+		}
+	default:
+		for i := 0; i < vb.Len(); i++ {
+			bi := vb.Index(i)
+			if va.Int() < bi.Int() {
+				va = bi
+			}
+		}
+	}
+	return va.Interface()
+}
+
+func MinInt(a int, b ...int) int { return min(a, b).(int) }
+func MaxInt(a int, b ...int) int { return max(a, b).(int) }
+
+func MinInt8(a int8, b ...int8) int8 { return min(a, b).(int8) }
+func MaxInt8(a int8, b ...int8) int8 { return max(a, b).(int8) }
+
+func MinInt16(a int16, b ...int16) int16 { return min(a, b).(int16) }
+func MaxInt16(a int16, b ...int16) int16 { return max(a, b).(int16) }
+
+func MinInt64(a int64, b ...int64) int64 { return min(a, b).(int64) }
+func MaxInt64(a int64, b ...int64) int64 { return max(a, b).(int64) }
+
+func MinUint(a uint, b ...uint) uint { return min(a, b).(uint) }
+func MaxUint(a uint, b ...uint) uint { return max(a, b).(uint) }
+
+func MinUint8(a uint8, b ...uint8) uint8 { return min(a, b).(uint8) }
+func MaxUint8(a uint8, b ...uint8) uint8 { return max(a, b).(uint8) }
+
+func MinUint16(a uint16, b ...uint16) uint16 { return min(a, b).(uint16) }
+func MaxUint16(a uint16, b ...uint16) uint16 { return max(a, b).(uint16) }
+
+func MinUint64(a uint64, b ...uint64) uint64 { return min(a, b).(uint64) }
+func MaxUint64(a uint64, b ...uint64) uint64 { return max(a, b).(uint64) }
---
+++ misc/misc_test.go
@@ -0,0 +1,39 @@
+package misc
+
+import (
+	"testing"
+)
+
+func TestMin(t *testing.T) {
+	for i, test := range []struct {
+		a int
+		b []int
+		t int
+	}{
+		{1, []int{2, 3}, 1},
+		{-1, []int{-2, -3}, -3},
+	} {
+		if o := MinInt(test.a, test.b...); o != test.t {
+			t.Errorf("%d %d != %d", i, test.t, o)
+		} else {
+			t.Logf("%d ok", i)
+		}
+	}
+}
+
+func TestMax(t *testing.T) {
+	for i, test := range []struct {
+		a int8
+		b []int8
+		t int8
+	}{
+		{1, []int8{2, 3}, 3},
+		{-1, []int8{-2, -3}, -1},
+	} {
+		if o := MaxInt8(test.a, test.b...); o != test.t {
+			t.Errorf("%d %d != %d", i, test.t, o)
+		} else {
+			t.Logf("%d ok", i)
+		}
+	}
+}
